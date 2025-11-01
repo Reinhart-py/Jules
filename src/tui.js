0c2c5484dc52559773777151ca196b4100606551
Reinhart-py <reinhart.py@gmail.com>
2025-11-01 16:27:15 +0600

    feat: Complete TUI implementation with all features

--- src/tui.js
+++ src/tui.js
@@ -109,6 +109,24 @@ const addSessionForm = blessed.form({
   });
 
   const saveButton = blessed.button({ parent: addSessionForm, name: 'save', content: 'Save', top: 5, left: 2, width: 10, height: 1, style: { bg: 'blue', focus: { bg: 'red' } } });
+  const backButton = blessed.button({ parent: addSessionForm, name: 'back', content: 'Back', top: 5, left: 15, width: 10, height: 1, style: { bg: 'blue', focus: { bg: 'red' } } });
+
+  saveButton.on('press', async () => {
+    const sessionName = sessionNameInput.getValue();
+    if (sessionName) {
+      logViewer.log(`Starting session: ${sessionName}`);
+      addSessionForm.hide();
+      screen.render();
+      try {
+        const sock = await connectToWhatsApp(sessionName);
+        sessions[sessionName] = sock;
+        logViewer.log(`Session '${sessionName}' added.`);
+      } catch (err) {
+        logViewer.log(`Error: ${err.message}`);
+      }
+    }
+  });
+
   const checkerOptionsForm = blessed.form({
     parent: screen,
     label: 'Checker Options',
@@ -481,27 +499,6 @@ const addSessionForm = blessed.form({
     screen.render();
   }
 });
-
-  saveButton.on('press', async () => {
-    const sessionName = sessionNameInput.getValue();
-    if (sessionName) {
-      logViewer.log(`Starting session: ${sessionName}`);
-      addSessionForm.hide();
-      screen.render();
-      try {
-        const sock = await connectToWhatsApp(sessionName);
-        sessions[sessionName] = sock;
-        logViewer.log(`Session '${sessionName}' added.`);
-      } catch (err) {
-        logViewer.log(`Error: ${err.message}`);
-      }
-    }
-  });
-
-  backButton.on('press', () => {
-    addSessionForm.hide();
-    screen.render();
-  });
 
 menu.focus();
 
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-node index.js
+node src/tui.js
